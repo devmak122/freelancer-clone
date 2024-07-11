84137fbb0a354a540262c369eacb81c13861ca62
devmak122 <devm37878@gmail.com>
2024-07-12 05:20:33 +0530

    update frontend

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -1,24 +1,24 @@
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
+  const jwt = require("jsonwebtoken");
+  const dotenv = require("dotenv");
 
-// Load config
-dotenv.config();
+  // Load config
+  dotenv.config();
 
-const JWT_SECRET = process.env.JWT_SECRET;
+  const JWT_SECRET = process.env.JWT_SECRET;
 
-const fetchuser = async (req, res, next) => {
-  const token = req.header("auth-token");
-  if (!token) {
-    return res.status(401).json({ error: "Token not provided" });
-  }
-  try {
-    const data = jwt.verify(token, JWT_SECRET);
-    req.user = data.user;
-    next();
-  } catch (error) {
-    console.error("Error verifying token:", error.message);
-    return res.status(401).json({ error: "Invalid or expired token" });
-  }
-};
+  const fetchuser = async (req, res, next) => {
+    const token = req.header("auth-token");
+    if (!token) {
+      return res.status(401).json({ error: "Token not provided" });
+    }
+    try {
+      const data = jwt.verify(token, JWT_SECRET);
+      req.user = data.user;
+      next();
+    } catch (error) {
+      console.error("Error verifying token:", error.message);
+      return res.status(401).json({ error: "Invalid or expired token" });
+    }
+  };
 
-module.exports = fetchuser;
+  module.exports = fetchuser;
