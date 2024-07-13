b60314c95b75f1a2e292bcf370d63cea5337a327
Ritu Makwana <RITUU.PCSI@GMAIL.COM>
2024-07-13 23:38:28 +0530

    update

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -12,7 +12,7 @@ const SignUp = () => {
     const { name, email, password } = credentials;
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/createuser", {
+      const response = await fetch("https://freelancer-clone-backend.onrender.com/api/auth/createuser", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
